3fdbe69ddc92133ae603868329e26d0271a090e9
kukuasir <kukuasir@gmail.com>
2018-06-15 10:41:22 +0800

    修正编译警告

--- src/custom/defines_post.hpp
+++ src/custom/defines_post.hpp
@@ -7,13 +7,7 @@
  * rAthena configuration file (http://rathena.org)
  * For detailed guidance on these check http://rathena.org/wiki/SRC/config/
  **/
-//客户端登陆器版本
-#define PACKETVER 20180124
 
-//封包加密的3个秘钥
-#define PACKET_OBFUSCATION_KEY1 0x2124cb58
-#define PACKET_OBFUSCATION_KEY2 0x2bd64269
-#define PACKET_OBFUSCATION_KEY3 0x10823ff3
 
 
 #endif // _CONFIG_CUSTOM_DEFINES_POST_HPP_
