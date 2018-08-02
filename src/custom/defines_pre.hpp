83fd5d5e55ee4060f00e3ae53c5d40c603f782b0
kukuasir <kukuasir@gmail.com>
2018-08-02 08:44:03 +0800

    改用20180321的客户端

--- src/custom/defines_pre.hpp
+++ src/custom/defines_pre.hpp
@@ -9,12 +9,14 @@
  * For detailed guidance on these check http://rathena.org/wiki/SRC/config/
  **/
  //客户端登陆器版本
-#define PACKETVER 20180124
+//#define PACKETVER 20180124
+#define PACKETVER 20180321
 
  //封包加密的3个秘钥
+/*
 #define PACKET_OBFUSCATION_KEY1 0x2124cb58
 #define PACKET_OBFUSCATION_KEY2 0x2bd64269
 #define PACKET_OBFUSCATION_KEY3 0x10823ff3
-
+*/
 
 #endif // _CONFIG_CUSTOM_DEFINES_PRE_HPP_
