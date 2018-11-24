3968f1a8f3427873b107113d548811e6ac0eecf5
kukuasir <kukuasir@163.com>
2018-11-24 17:26:53 +0800

    修复生命体自动进食无效的bug,同时恢复进食百分比选项(homunculus_autofeed_rate)

--- src/map/homunculus.cpp
+++ src/map/homunculus.cpp
@@ -891,6 +891,11 @@ static TIMER_FUNC(hom_hungry){
 		clif_emotion(&hd->bl, ET_OK);
 	}
 
+	//自动进食 [kuku]
+	if (battle_config.feature_homunculus_autofeed && hd->homunculus.autofeed && hd->homunculus.hunger <= battle_config.feature_homunculus_autofeed_rate) {
+		hom_food(sd, hd);
+	}
+
 	if (hd->homunculus.hunger < 0) {
 		hd->homunculus.hunger = 0;
 		// Delete the homunculus if intimacy <= 100
